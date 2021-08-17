5cc26c5a2b2ec2de8f690038fb3bb2b5a69d0ddf
unknown <shabaneissa767@gmail.com>
2021-08-17 20:24:06 +0200

    Updates

---
+++ precache-manifest.b17f03262f8d3aa60e82f001598dcc2c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c20bde4a745927e00b93",
+    "url": "./static/css/main.2f5da8a9.chunk.css"
+  },
+  {
+    "revision": "c20bde4a745927e00b93",
+    "url": "./static/js/main.c20bde4a.chunk.js"
+  },
+  {
+    "revision": "87f7407a67ef306dbf94",
+    "url": "./static/js/1.87f7407a.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "85a28de88618162b7166d77169f7ec07",
+    "url": "./index.html"
+  }
+];
